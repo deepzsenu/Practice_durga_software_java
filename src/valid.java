fa9c4cb16d018a8f0f0eb676fbcc8247469cb78d
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-26 09:09:43 +0530

    practicing java

---
+++ src/valid.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class valid{
+    public static void main(String[] args) {
+        int String = 888;
+        int Runable  = 999;
+        // String and Runable are valid identifiers but not recommended becuse the
+        // the readability of the code becomes difficult
+        System.out.println(String+","+Runable);
+   }
+}
