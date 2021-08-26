ec771ff7e19ef62ded4158fb6f113e699dbd22c7
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-26 10:57:11 +0530

    practicing java

---
+++ src/lecture2/datatype.java
@@ -0,0 +1,15 @@
+import  java.io.*;
+import  java.util.*;
+//package lecture2;
+public class datatype{
+    public static void main(String[] args) {
+        int x = 9; // integer type
+        short y = 127;
+        long t = 34;
+
+        /* char n = "h" ; */
+        boolean h =true;
+        System.out.println(t);
+
+    }
+}
