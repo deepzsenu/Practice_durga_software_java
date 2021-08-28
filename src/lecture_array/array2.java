532af43fc8338ae6c5b9075a64788ef40adbad8d
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-28 09:41:16 +0530

    practicing java

--- src/lecture_array/array2.java
+++ src/lecture_array/array2.java
@@ -2,6 +2,8 @@ package lecture_array;
 
 public class array2 {
     public static void main(String[] args) {
+        int d ;
+        d  = 5;
 
     }
 }
---
+++ src/lecture_array/array2d.java
@@ -0,0 +1,17 @@
+package lecture_array;
+
+public class array2d {
+    public static void main(String[] args) {
+        // using array of array approach
+        int[][] x = new int[3][3];
+        x[0] = new int[2];// the length of second column by default is 2
+
+        x[1] = new int[1];  // the lenght of third column by defautl is 1
+        // the lenght of third column by default is 3
+
+        System.out.println(x[2].length);
+
+    }
+
+
+}
