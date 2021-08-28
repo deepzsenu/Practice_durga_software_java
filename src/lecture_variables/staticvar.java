5744dc59bea5451db07053816249aa2e06a3d103
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-28 16:06:14 +0530

    practicing java

--- src/lecture_variables/staticvar.java
+++ src/lecture_variables/staticvar.java
@@ -4,6 +4,8 @@ public class staticvar {
     static int x =123;
 
     public static void main(String[] args) {
+        staticvar s = new staticvar();
+        System.out.println(s.x);
         System.out.println(staticvar.x);
         System.out.println(x);
     }
