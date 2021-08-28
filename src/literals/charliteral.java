53b5b3709a7e4fb82146d8d2e17861c6a3b3778a
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-28 07:21:13 +0530

    practicing java

---
+++ src/literals/charliteral.java
@@ -0,0 +1,11 @@
+package literals;
+
+public class charliteral {
+    public static void main(String[] args) {
+        char ch = 'a'; //valid
+        char ch = a ; //not valid valiable a not found
+        char ch = "a"; // not valid found string required char
+        char ch = 'ab'// not valid return error unclosed char literal
+
+    }
+}
