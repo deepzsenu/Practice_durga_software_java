b743c2d899ad63bf5a26350a1f2df7ffbfacc758
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-28 16:04:44 +0530

    practicing java

---
+++ src/lecture_variables/staticvar.java
@@ -0,0 +1,10 @@
+package lecture_variables;
+
+public class staticvar {
+    static int x =123;
+
+    public static void main(String[] args) {
+        System.out.println(staticvar.x);
+        System.out.println(x);
+    }
+}
