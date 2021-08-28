ebe1d5aad535208af8ca5231094150065e4219d9
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-28 15:21:02 +0530

    practicing java

---
+++ src/lecture_array/vararray.java
@@ -0,0 +1,13 @@
+package lecture_array;
+
+public class vararray {
+    public static void main(String[] args) {
+        String[] argh = { "A", "RF","eed"};
+        args = argh;
+        for( String s : args)
+        {
+            System.out.println(s);
+        }
+
+    }
+}
