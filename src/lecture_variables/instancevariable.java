a7d7853cee8356124214d326faca5314b39cc9fc
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-28 15:59:25 +0530

    practicing java

---
+++ src/lecture_variables/instancevariable.java
@@ -0,0 +1,14 @@
+package lecture_variables;
+
+public class instancevariable {
+    int x = 100;
+    public static void main(String[] args) {
+        //System.out.println(x);// give compilation error coz we cann't call instant variable in static function
+        instancevariable i = new instancevariable();
+        System.out.println(i.x); // prints the value of x
+    }
+    public void main()
+    {
+        System.out.println(x);// is valid
+    }
+}
