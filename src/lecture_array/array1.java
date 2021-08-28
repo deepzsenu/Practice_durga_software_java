db3b95a27fa5a028820d58bef7b7da8e7cd9f675
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-28 08:42:45 +0530

    practicing java

---
+++ src/lecture_array/array1.java
@@ -0,0 +1,22 @@
+package lecture_array;
+
+public class array1 {
+    public static void main(String[] args) {
+        // types off array
+
+        int[] a = new int[3];
+        int[][] b = new int[3][2];
+        double[] c = new double[3];
+        short[] d = new short[3];
+        byte[] e = new byte[3];
+        boolean[] f = new boolean[3];
+        char[] g = new char[3];
+        System.out.println(a.getClass().getName());
+        System.out.println(b.getClass().getName());
+        System.out.println(c.getClass().getName());
+        System.out.println(d.getClass().getName());
+        System.out.println(e.getClass().getName());
+        System.out.println(f.getClass().getName());
+        System.out.println(g.getClass().getName());
+     }
+}
