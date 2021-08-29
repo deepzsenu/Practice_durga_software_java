2d1a5e23bc6e1016de0ccb658d9a1284573cb6b5
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-29 10:20:24 +0530

    practicing java

---
+++ src/lecture_variables/localvar.java
@@ -0,0 +1,16 @@
+package lecture_variables;
+
+public class localvar {
+    public static void main(String[] args) {
+        int x ;
+        if ( args.length> 0)
+        {
+            x= 10;
+                    }
+        else
+        {
+            x =30;
+        }
+        System.out.println(x);
+    }
+}
