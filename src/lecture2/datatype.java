3c3d1d850dac7ddfa74fda9a997116e7ada0ab6b
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-08-30 18:51:00 +0530

    practicing java

--- src/lecture2/datatype.java
+++ src/lecture2/datatype.java
@@ -1,6 +1,6 @@
 import  java.io.*;
 import  java.util.*;
-//package lecture2;
+package lecture2;
 public class datatype{
     public static void main(String[] args) {
         int x = 9; // integer type
--- src/main_method/parentss.java
+++ src/main_method/parentss.java
@@ -9,6 +9,7 @@ public class parentss {
 
 class childs extends parentss{
     public static void main(String[] args) {
+
         System.out.println("This is chhild class");
     }
 
